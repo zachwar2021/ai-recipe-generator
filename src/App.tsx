64193f93e5dd6ed96a95d454e1ea786cccc18168
Zachary Warren <zacharywarren2021@gmail.com>
2026-04-23 17:16:42 -0500

    fix type imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { FormEvent, useState } from "react";
+import { type FormEvent, useState } from "react";
 import { Loader, Placeholder } from "@aws-amplify/ui-react";
 import "./App.css";
 import { Amplify } from "aws-amplify";
-import { Schema } from "../amplify/data/resource";
+import { type Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
