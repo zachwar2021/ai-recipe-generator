834e01764ea3972d8536cdafbbe18c84488894ca
Zachary Warren <zacharywarren2021@gmail.com>
2026-04-23 17:31:22 -0500

    please work?

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+    'process.env': {},
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@aws-amplify/backend'],
+  },
+  ssr: {
+    noExternal: ['@aws-amplify/ui-react'],
+  },
 })
